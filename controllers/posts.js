c4bed8a554a66acb7dd41c8aa78c0a6d7affbbc1
Christopher Chang <chris.chang1989@gmail.com>
2024-08-01 12:46:29 -0700

    Incrementing changes: successful changes of pre-existing fields - Title and Caption to Date and Odometer

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,8 @@
-const cloudinary = require("../middleware/cloudinary");
-const Post = require("../models/Post");
+const cloudinary = require('../middleware/cloudinary');
+const Post = require('../models/Post');
 
 module.exports = {
+  // 8/1/24: Loads profile page
   getProfile: async (req, res) => {
     try {
       // 9:10:00: Since we have a session each request (req) contains the logged-in users' info: req.user
@@ -9,12 +10,13 @@ module.exports = {
       // Grabbing just the posts of the logged-in user
       const posts = await Post.find({ user: req.user.id });
       // 9:12:20: Sending post data from MongoDb and user data to ejs template
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      res.render('profile.ejs', { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
     }
   },
-// 9:16:00: Deleted getFeed promise
+  // 8/1/24: Loads individual post
+  // 9:16:00: Deleted getFeed promise
   getPost: async (req, res) => {
     try {
       // 9:16:30: id parameter comes from the poste routes
@@ -22,11 +24,13 @@ module.exports = {
       // Example URL: http://localhost:2121/post/65ea45ab462fec3e04252f30
       // id === 65ea45ab462fec3e04252f30
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      res.render('post.ejs', { post: post, user: req.user });
     } catch (err) {
       console.log(err);
     }
   },
+
+  // 8/1/24: Creates post - match fields to models and profile ejs
   createPost: async (req, res) => {
     try {
       // Upload image to cloudinary
@@ -34,15 +38,14 @@ module.exports = {
 
       // 9:19:10: Media is stored on cloudinary - the above request responds with url to media and the media id that you will need when deleting content
       await Post.create({
-        title: req.body.title,
+        date: req.body.date,
         image: result.secure_url,
         cloudinaryId: result.public_id,
-        caption: req.body.caption,
-        likes: 0,
+        odometer: req.body.odometer,
         user: req.user.id,
       });
-      console.log("Post has been added!");
-      res.redirect("/profile");
+      console.log('New refuel added!');
+      res.redirect('/profile');
     } catch (err) {
       console.log(err);
     }
@@ -55,7 +58,7 @@ module.exports = {
           $inc: { likes: 1 },
         }
       );
-      console.log("Likes +1");
+      console.log('Likes +1');
       res.redirect(`/post/${req.params.id}`);
     } catch (err) {
       console.log(err);
@@ -69,10 +72,10 @@ module.exports = {
       await cloudinary.uploader.destroy(post.cloudinaryId);
       // Delete post from db
       await Post.remove({ _id: req.params.id });
-      console.log("Deleted Post");
-      res.redirect("/profile");
+      console.log('Deleted Post');
+      res.redirect('/profile');
     } catch (err) {
-      res.redirect("/profile");
+      res.redirect('/profile');
     }
   },
 };
