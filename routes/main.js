d15bd65a4fa352bd4c8f54f7d2a8535e491f8f65
Christopher Chang <chris.chang1989@gmail.com>
2024-08-05 14:39:53 -0700

    Forgot to save changes in main.js for previous commit

--- routes/main.js
+++ routes/main.js
@@ -15,8 +15,8 @@ router.post('/login', authController.postLogin);
 router.get('/logout', authController.logout);
 
 // Route for intro - 8/5/24: don't think I need this since I just added boolean for checking if user completed setup to bring intro - nvm think I do need since I have the intro post in my "intro" view
-router.post('/intro', authController.postLogin);
-router.get('/intro', authController.getLogin);
+router.post('/intro', ensureAuth, authController.postLogin);
+router.get('/intro', ensureAuth, authController.getLogin);
 
 // 8/5/24: Might not need below since I replaced with intro above - nvm need since clicking signup leads to /signup, but I think I can add the checks for if user entered car details in the "getSignup" and "postSignup" methods
 router.get('/signup', authController.getSignup);
