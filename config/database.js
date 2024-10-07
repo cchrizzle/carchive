7fe085fd8f16d7849fbcb8b702e49c2f6c16d971
Christopher Chang <chris.chang1989@gmail.com>
2024-10-07 14:36:31 -0700

    Changed database name to "v2"

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
-      dbName: 'v1',
+      dbName: 'v2',
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
